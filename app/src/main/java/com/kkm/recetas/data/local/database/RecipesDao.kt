a423f7740db140717e9aafbb0eef0687d059f228
VictorDVLP <crespo.9.11.88@gmail.com>
2024-05-21 00:01:25 +0200

    Practice 1

--- app/src/main/java/com/kkm/recetas/data/local/database/RecipesDao.kt
+++ app/src/main/java/com/kkm/recetas/data/local/database/RecipesDao.kt
@@ -3,6 +3,7 @@ package com.kkm.recetas.data.local.database
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
@@ -11,7 +12,7 @@ interface RecipesDao {
     @Query("SELECT * FROM recipes")
     fun getLocalRecipes(): Flow<List<LocalRecipes>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRecipe(recipe: LocalRecipes)
 
     @Delete
