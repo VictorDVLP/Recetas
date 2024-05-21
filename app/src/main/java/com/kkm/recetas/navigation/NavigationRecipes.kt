8f2701a14d3a3e4f3f00d530719afbfcdf1c89b9
VictorDVLP <crespo.9.11.88@gmail.com>
2024-05-21 15:50:25 +0200

    Update Navigation with safe type

--- app/src/main/java/com/kkm/recetas/navigation/NavigationRecipes.kt
+++ app/src/main/java/com/kkm/recetas/navigation/NavigationRecipes.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.kkm.recetas.repository.RecipesRepository
 import com.kkm.recetas.ui.screens.DetailScreen
 import com.kkm.recetas.ui.screens.RecipesScreen
@@ -13,14 +14,15 @@ import com.kkm.recetas.ui.screens.RecipesScreen
 fun NavigationRecipes(repository: RecipesRepository) {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "recipes") {
-        composable("recipes") {
-            RecipesScreen( repository = repository ) { navController.navigate("detail/$it") }
+    NavHost(navController = navController, startDestination = Home) {
+        composable<Home> {
+            RecipesScreen( repository = repository ) { navController.navigate(RecipeDetail(it)) }
         }
 
-        composable("detail/{id}") {
-            val id = it.arguments?.getString("id") ?: ""
-            DetailScreen( id = id, repository = repository ) { navController.popBackStack() }
+        composable<RecipeDetail> { backStackEntry ->
+            val detail = backStackEntry.toRoute<RecipeDetail>()
+            DetailScreen( id = detail.id, repository = repository )
+            { navController.popBackStack() }
         }
     }
 }
