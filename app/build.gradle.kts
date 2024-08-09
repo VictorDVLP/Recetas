08512eaf3e16f6ed1dda9929bc00c01c827c4c4e
VictorDVLP <crespo.9.11.88@gmail.com>
2024-08-09 23:03:41 +0200

    Update version AGP and Navigation Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.kkm.architectureclean.recipes"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kkm.architectureclean.recipes"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
